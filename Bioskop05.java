866161f403cea09bef327c78ccdec672c823bc4b
oninonins <varelinobubadibako11@gmail.com>
2024-11-07 13:16:53 +0700

    new

---
+++ Bioskop05.java
@@ -0,0 +1,30 @@
+public class Bioskop05 {
+
+    public static void main(String[] args) {
+        
+        String[][] penonton = new String[5][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (String i: penonton[2]) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ",penonton[i]));
+        }
+        //System.out.println(penonton[0].length);
+        //System.out.println(penonton[1].length);
+        //System.out.println(penonton[2].length);
+        //System.out.println(penonton[3].length);
+    }
+}
